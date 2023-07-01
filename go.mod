580adedb8a97c291d108f7f9aae6020c305c576b
Johannes Bareuther <none@example.om>
2023-07-02 00:55:44 +0200

    remove all font and image related code

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/johbar/go-poppler
 
 go 1.18
-
-require github.com/ungerik/go-cairo v0.0.0-20220815093914-e24bd4259cef
--- page.go
+++ page.go
@@ -6,7 +6,6 @@ package poppler
 import "C"
 import (
 	"runtime"
-	"unsafe"
 )
 
 //import "fmt"
@@ -17,33 +16,6 @@ type Page struct {
 
 func (p *Page) Text() string {
 	return C.GoString(C.poppler_page_get_text(p.p))
-}
-
-func (p *Page) TextAttributes() (results []TextAttributes) {
-	a := C.poppler_page_get_text_attributes(p.p)
-	defer C.poppler_page_free_text_attributes(a)
-	var attr *C.PopplerTextAttributes
-	results = make([]TextAttributes, 0)
-	el := C.g_list_first(a)
-	for el != nil {
-		attr = (*C.PopplerTextAttributes)(el.data)
-		fn := *attr.font_name
-		result := TextAttributes{
-			FontName:     toString(&fn),
-			FontSize:     float64(attr.font_size),
-			IsUnderlined: toBool(attr.is_underlined),
-			StartIndex:   int(attr.start_index),
-			EndIndex:     int(attr.end_index),
-			Color: Color{
-				R: int(attr.color.red),
-				G: int(attr.color.green),
-				B: int(attr.color.blue),
-			},
-		}
-		results = append(results, result)
-		el = el.next
-	}
-	return
 }
 
 func (p *Page) Size() (width, height float64) {
@@ -64,75 +36,6 @@ func (p *Page) Duration() float64 {
 	return float64(C.poppler_page_get_duration(p.p))
 }
 
-func (p *Page) Images() (results []Image) {
-	l := C.poppler_page_get_image_mapping(p.p)
-	defer C.poppler_page_free_image_mapping(l)
-	results = make([]Image, 0)
-	var im *C.PopplerImageMapping
-	for el := C.g_list_first(l); el != nil; el = el.next {
-		im = (*C.PopplerImageMapping)(el.data)
-		result := Image{
-			Id: int(im.image_id),
-			Area: Rectangle{
-				X1: float64(im.area.x1),
-				Y1: float64(im.area.y1),
-				X2: float64(im.area.x2),
-				Y2: float64(im.area.y2),
-			},
-			p: p.p,
-		}
-		results = append(results, result)
-	}
-	return
-}
-
-func (p *Page) TextLayout() (layouts []Rectangle) {
-	var rect *C.PopplerRectangle
-	var n C.guint
-	if toBool(C.poppler_page_get_text_layout(p.p, &rect, &n)) {
-		defer C.g_free((C.gpointer)(rect))
-		layouts = make([]Rectangle, int(n))
-		r := (*[1 << 30]C.PopplerRectangle)(unsafe.Pointer(rect))[:n:n]
-		for i := 0; i < int(n); i++ {
-			layouts[i] = Rectangle{
-				X1: float64(r[i].x1),
-				Y1: float64(r[i].y1),
-				X2: float64(r[i].x2),
-				Y2: float64(r[i].y2),
-			}
-		}
-	}
-	return
-}
-
-func (p *Page) TextLayoutAndAttrs() (result []TextEl) {
-	text := p.Text()
-	attrs := p.TextAttributes()
-	layout := p.TextLayout()
-	result = make([]TextEl, len(layout))
-	attrsRef := make([]*TextAttributes, len(attrs))
-	for i, a := range attrs {
-		attr := a
-		attrsRef[i] = &attr
-	}
-	i := 0
-	for _, t := range text {
-		var a *TextAttributes
-		for _, a = range attrsRef {
-			if i >= a.StartIndex && i <= a.EndIndex {
-				break
-			}
-		}
-		result[i] = TextEl{
-			Text:  string(t),
-			Attrs: a,
-			Rect:  layout[i],
-		}
-		i++
-	}
-	return
-}
-
 //Close frees memory allocated when Poppler opened the page
 func (p *Page) Close() {
 	//GC/finalizer shouldn't try to free C memory that has been freed already
@@ -140,6 +43,6 @@ func (p *Page) Close() {
 	closePage(p)
 }
 
-func closePage (p *Page) {
+func closePage(p *Page) {
 	C.g_object_unref(C.gpointer(p.p))
 }
